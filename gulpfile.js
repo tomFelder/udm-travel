55283391ce0ed51a699341546e3948099e32dcce
tomFelder <tom.frauenfelder@gmail.com>
2017-07-07 23:06:15 +1000

    resolved issue with GULP

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,23 @@ cssImport = require('postcss-import'),
 browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
-  console.log('Hooray - you created a GULP task')
+  console.log("Hooray - you created a Gulp task.");
 });
 
 gulp.task('html', function() {
-  console.log('imagine something useful here')
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
 
   browserSync.init({
+    notify: false,
     server: {
       baseDir: "app"
     }
@@ -36,10 +37,10 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
-  
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('.app/temp/styles/styles.css')
+  return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
