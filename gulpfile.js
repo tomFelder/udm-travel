00eb6091f0dc9d6460bd4b52af3a407eb67fbe98
tomFelder <tom.frauenfelder@gmail.com>
2017-07-07 22:56:47 +1000

    updated CSS and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log('Hooray - you created a GULP task')
@@ -15,16 +17,29 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
+  
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('.app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
