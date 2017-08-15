b86efdb773f2bf329fbbe0c725c4c011510415e2
tomFelder <tom.frauenfelder@gmail.com>
2017-08-15 16:59:33 +1000

    integrate babel

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,18 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
+
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+jane.payTaxes();
 
 $("h1").remove();
