7f3529a0018eac3b03abc604c4af4592821f8ba2
tomFelder <tom.frauenfelder@gmail.com>
2017-09-30 12:10:03 +1000

    working on modal

---
+++ app/assets/scripts/modules/modal.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+class Modal {
+  constuctor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
+  }
+
+  events() {
+    // clicking the open modal button
+
+    // clicking the x close modal button
+
+    // pushes escape key
+  }
+
+  openModal() {
+
+  }
+
+  closeModal() {
+
+  }
+}
+
+export default Modal;
