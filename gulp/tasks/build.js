0237e255e9c4e2cb46b298155dd02f54bda99985
tomFelder <tom.frauenfelder@gmail.com>
2017-10-10 20:00:40 +1100

    updated build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require(del),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify');
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
@@ -19,7 +22,10 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 
 gulp.task('usemin', ['deleteDistFolder'], function() {
   return gulp.src('./app/index.html')
-    .pipe
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}],
+      js: [function() {return rev()}, function() {return uglify()}]
+    }))
     .pipe(gulp.dest('./dist'));
 });
 
